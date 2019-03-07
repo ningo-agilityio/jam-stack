f06b9eaa7e16601f111b583f7f517e84f65ea005
Ni Ngo <ni.ngo@asnet.com.vn>
2019-03-07 11:26:44 +0700

    Query data from contentful by graphql

--- blog-posts/src/components/layout.js
+++ blog-posts/src/components/layout.js
@@ -11,6 +11,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import 'materialize-css/dist/css/materialize.min.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
--- blog-posts/src/pages/index.js
+++ blog-posts/src/pages/index.js
@@ -1,19 +1,57 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const query = graphql`
+  query ListPosts {
+    allContentfulPost {
+      edges {
+        node {
+          id
+          title
+          content {
+            content
+          }
+          createdDate
+          author {
+            name
+          }
+        }
+      }
+    }
+  }
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <StaticQuery 
+      query={query}
+      render={(data) => {
+        return (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.allContentfulPost.edges.map((post) => (
+                  <tr key={post.node.id}>
+                    <td>{post.node.title}</td>
+                    <td>{post.node.author.name}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        )
+      }}
+    />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
